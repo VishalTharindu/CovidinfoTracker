b97dc712b041a98ab408dfbec3d0270fa888692f
Vishal <40030063+VishalTharindu@users.noreply.github.com>
2020-05-25 00:51:59 +0530

    Updates

---
+++ precache-manifest.31e597a3658ab979611061fe085201dc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb2670c4e9a2028da6fd91a4d3ab182e",
+    "url": "/CovidinfoTracker/index.html"
+  },
+  {
+    "revision": "12cc54f1d1b73a4730f8",
+    "url": "/CovidinfoTracker/static/css/main.dd0a3df3.chunk.css"
+  },
+  {
+    "revision": "726dded1da3eb1a1d3be",
+    "url": "/CovidinfoTracker/static/js/2.5bb6aee2.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/CovidinfoTracker/static/js/2.5bb6aee2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12cc54f1d1b73a4730f8",
+    "url": "/CovidinfoTracker/static/js/main.30445a5d.chunk.js"
+  },
+  {
+    "revision": "fe781f0aaab282721b09",
+    "url": "/CovidinfoTracker/static/js/runtime-main.fa29a80b.js"
+  },
+  {
+    "revision": "af24b408738d8f97ff923f541893fe10",
+    "url": "/CovidinfoTracker/static/media/coronalabel.af24b408.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/CovidinfoTracker/static/media/logo.5d5d9eef.svg"
+  }
+]);
